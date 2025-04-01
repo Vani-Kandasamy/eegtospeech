4671a7248c69b58b3e5f1bc31b4ede032f20cfc7
Vani-Kandasamy <vani.kandasamy@pyxeda.ai>
2025-04-01 07:59:56 +0000

    error corrected

--- app.py
+++ app.py
@@ -78,7 +78,7 @@ def main():
             st.subheader("Concatenated Model Prediction")
             st.write(concatenated_labels)
             text = concatenated_labels
-    st.write(text)
+        st.write(text)
         if text and st.button("Convert and Play"):
             text_to_speech(text)
 
