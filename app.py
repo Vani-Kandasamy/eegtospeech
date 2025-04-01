0fbe0645cbdec3446158e644381716d5f5641c11
Vani-Kandasamy <vani.kandasamy@pyxeda.ai>
2025-04-01 07:25:37 +0000

    try block updated

--- app.py
+++ app.py
@@ -62,17 +62,17 @@ def main():
             if st.button("Convert and Play"):
                 
                 try:
-                        # Convert text to speech
-                        tts = gTTS(text=concatenated_labels, lang='en')
-                        # Save to a bytes buffer
-                        audio_buffer = BytesIO()
-                        tts.write_to_fp(audio_buffer)
+                    # Convert text to speech
+                    tts = gTTS(text=concatenated_labels, lang='en')
+                    # Save to a bytes buffer
+                    audio_buffer = BytesIO()
+                    tts.write_to_fp(audio_buffer)
 
-                        # Reset buffer position to start
-                        audio_buffer.seek(0)
-
-                        # Play the audio (in-app)
-                        st.audio(audio_buffer, format="audio/mp3")
+                    # Reset buffer position to start
+                    audio_buffer.seek(0)
+                        
+                    # Play the audio (in-app)
+                    st.audio(audio_buffer, format="audio/mp3")
 
                 except Exception as e:
                         st.error(f"An error occurred: {e}")
